76e629a2b24b6c331a673cd97e51005192e834ef
Moritz Hamm <moritz.hamm@vc-g.de>
2023-04-04 00:07:11 +0200

    style aenderung

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  login() {}
+  login() {
+    // findSportTeams(sportId: number) {
+    //   this.activeteamlist.length = 0;
+    //   fetch('http://localhost:1337/api/sports/' + sportId + '?populate=*', {
+    //     method: 'GET',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //   })
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //       for (const event of data.data.attributes.teams.data) {
+    //         this.activeteamlist.push({
+    //           id: event.id,
+    //           name: event.attributes.name,
+    //           gender: event.attributes.gender,
+    //           agegroup: event.attributes.agegroup,
+    //         });
+    //       }
+    //       this.teamDataSource.data = this.activeteamlist;
+    //     });
+    // }
+    console.log();
+  }
 }
